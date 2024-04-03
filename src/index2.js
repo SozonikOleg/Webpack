060be796235219db6a225e11581f2b2fcef15001
Oleg Sozonik <sozonik999@gmail.com>
2024-04-04 00:43:25 +0300

    initial commit

---
+++ src/index2.js
@@ -0,0 +1,4 @@
+import {sum} from './test';
+
+console.log("Helllo2")
+console.log(sum(2,2));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => {
+    return {
+    mode: env.mode,
+    entry:{
+        helloWorld:   path.resolve(__dirname, 'src/index.js'),
+        // helloWorld2:   path.resolve(__dirname, 'src/index2.js'),
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')})
+        ]
+}};
