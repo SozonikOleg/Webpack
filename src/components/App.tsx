1aa8391d5b358f687cd0da81dd9f7842ff04c423
Oleg Sozonik <sozonik999@gmail.com>
2024-04-18 10:34:34 +0300

    add: scss

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import './App.scss'
 
 export const App = () => {
-    return <div>Hello !</div>
+    return <div> <button>
+        <span>Click</span>
+      </button></div>
 }
